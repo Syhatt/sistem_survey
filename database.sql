fe31c698f278546b038d2ae58c62926793cbd500
krisnayana19 <65766534+krisnayana19@users.noreply.github.com>
2023-05-30 13:51:17 +0700

    membenarkan auto increment dan relasi

--- database.sql
+++ database.sql
@@ -1,39 +1,135 @@
--- Membuat basis data untuk sistem survey
-CREATE DATABASE sistem_survey;
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: sistem_survey
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
 
--- Menggunakan basis data yang baru dibuat
-USE sistem_survey;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Tabel untuk menyimpan survei
-CREATE TABLE Surveys (
-  id INT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  created_at DATETIME NOT NULL
-);
+--
+-- Table structure for table `likertscale`
+--
 
--- Tabel untuk menyimpan pertanyaan survei
-CREATE TABLE Questions (
-  id INT PRIMARY KEY,
-  survey_id INT,
-  question_text TEXT NOT NULL,
-  FOREIGN KEY (survey_id) REFERENCES Surveys(id)
-);
+DROP TABLE IF EXISTS `likertscale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likertscale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `scale_value` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Tabel untuk menyimpan skala Likert
-CREATE TABLE LikertScale (
-  id INT PRIMARY KEY,
-  scale_value INT NOT NULL,
-  description VARCHAR(255) NOT NULL
-);
+--
+-- Dumping data for table `likertscale`
+--
 
--- Tabel untuk menyimpan jawaban survei
-CREATE TABLE SurveyResponses (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  survey_id INT,
-  question_id INT,
-  respondent_name VARCHAR(255) NOT NULL,
-  response_value INT,
-  response_date DATETIME,
-  FOREIGN KEY (survey_id) REFERENCES Surveys(id),
-  FOREIGN KEY (question_id) REFERENCES Questions(id)
-);
+LOCK TABLES `likertscale` WRITE;
+/*!40000 ALTER TABLE `likertscale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likertscale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_id` int(11) DEFAULT NULL,
+  `question_text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `survey_id` (`survey_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (3,5,'semangka manis'),(4,5,'kamu suka manis'),(8,7,'Tamfan'),(9,7,'Pemberani'),(10,7,'Pintar'),(11,8,'tamfan'),(12,8,'pemberani'),(13,8,'rajin'),(14,8,'kaya');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `surveyresponses`
+--
+
+DROP TABLE IF EXISTS `surveyresponses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `surveyresponses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `respondent_name` varchar(255) NOT NULL,
+  `response_value` int(11) DEFAULT NULL,
+  `response_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  KEY `survey_id` (`survey_id`),
+  CONSTRAINT `surveyresponses_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `surveyresponses_ibfk_2` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `surveyresponses`
+--
+
+LOCK TABLES `surveyresponses` WRITE;
+/*!40000 ALTER TABLE `surveyresponses` DISABLE KEYS */;
+INSERT INTO `surveyresponses` VALUES (8,5,3,'Gum',1,'2023-05-29 22:03:12'),(9,5,4,'Gum',1,'2023-05-29 22:03:16');
+/*!40000 ALTER TABLE `surveyresponses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `surveys`
+--
+
+DROP TABLE IF EXISTS `surveys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `surveys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `surveys`
+--
+
+LOCK TABLES `surveys` WRITE;
+/*!40000 ALTER TABLE `surveys` DISABLE KEYS */;
+INSERT INTO `surveys` VALUES (5,'semangka','0000-00-00 00:00:00'),(7,'Bos aldi','2023-05-29 22:41:20'),(8,'Bang saddam','2023-05-29 22:43:29');
+/*!40000 ALTER TABLE `surveys` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-30 13:49:39
